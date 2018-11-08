644a4b36800d32d572acc37db60455eb710a6fde
Kenneth Drake-Sargent <kennethrsargent@gmail.com>
2018-11-08 14:06:27 -0800

    ignored sql files added ignore file

--- authorviews.sql
+++
@@ -1,8 +0,0 @@
-select authors.name, count(authors.name) numViews from
-(
-select author, concat('/article/',slug) as slugPath from articles
-) as artJoin
-join log as l on artJoin.slugPath = l.path
-join authors on artJoin.author = authors.id
-group by name
-order by numViews desc;
--- mostpop.sql
+++
@@ -1,8 +0,0 @@
-select title, count(title) numViews from
-(
-select title, author, concat('/article/',slug) as slugPath from articles
-) as a
-join log as l on a.slugPath = l.path
-group by title
-order by numViews desc
-limit 3;
